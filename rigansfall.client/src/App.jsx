38630f1123d745a9598b3a41d1761698ac3b5d45
Bartek <baarti02@gmail.com>
2025-01-15 19:04:04 +0100

    Dodano paragraf do głównego widoku aplikacji.

--- rigansfall.client/src/App.jsx
+++ rigansfall.client/src/App.jsx
@@ -36,6 +36,7 @@ function App() {
             <div id="welcome">
                 <h1>Witaj w Rigansfall</h1>
                 <h2>Miejscu dla najodwazniejszych bohaterow!</h2>
+                <p>To jest test dla dzia³ania na ga³êziach githuba</p>
             </div>
 
             <h2 id="tableLabel">Weather forecast</h2>
