a74330a1a90c5eb06208b5122eff817d522eca93
adrianpiecek <adrian.piecek@gmail.com>
2025-01-15 22:38:00 +0100

    added maps controller and test new map generetor method

---
+++ Rigansfall.Server/Controllers/MapsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rigansfall.Server.Models;
+
+namespace Rigansfall.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MapsController : ControllerBase
+    {
+        [HttpGet("new-map")]
+        public ActionResult<Map> GetMap()
+        {
+            int mapID = getMapId();
+            // Przykładowa mapa 10x10
+            var map = new Map
+            {
+                Id = mapID,
+                Name = "Test Map",
+                Tiles = Enumerable.Range(0, 10)
+                    .SelectMany(x => Enumerable.Range(0, 10)
+                    .Select(y => new Tile
+                    {
+                        MapId = mapID,
+                        X = x,
+                        Y = y,
+                        isWalkable = true // Wszystkie kafelki na początku są "przechodnie"
+                    }))
+                    .ToList()
+            };
+
+            return Ok(map);
+            
+        }
+
+        private int getMapId()
+        {
+            return 1; // Na razie zawsze zwraca 1
+        }
+
+    }
+}
