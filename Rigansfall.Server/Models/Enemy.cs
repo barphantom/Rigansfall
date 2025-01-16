6688abfb170e2569913b7d2024ada9fbbf2832d4
sztorm84 <Dominik.Bronisz1234@gmail.com>
2025-01-17 00:31:52 +0100

    Dodano 2 nowe klasy ('Treasure' oraz 'Enemy'), zmodyfikowano klase 'Player'

---
+++ Rigansfall.Server/Models/Enemy.cs
@@ -0,0 +1,15 @@
+﻿namespace Rigansfall.Server.Models
+{
+    public class Enemy
+    {
+        public int ID { get; set; }
+        public int MapID { get; set; }
+        public string? Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int MaxHealth { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Resilience { get; set; }
+    }
+}
--- Rigansfall.Server/Models/Player.cs
+++ Rigansfall.Server/Models/Player.cs
@@ -5,9 +5,8 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public int MaxHealth { get; set; }
-        public int MaxSpeed { get; set; }
+        public int Resilience { get; set; }
         public int Defense { get; set; }
         public int Attack { get; set; }
-        public int XP { get; set; }
     }
 }
