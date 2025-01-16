4423a4d5d1cd1c4042f2393f329d78fa2af0daf2
sztorm84 <Dominik.Bronisz1234@gmail.com>
2025-01-17 00:57:45 +0100

    elo elo

--- Rigansfall.Server/Models/Player.cs
+++ Rigansfall.Server/Models/Player.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
         public int MaxHealth { get; set; }
         public int Resilience { get; set; }
         public int Defense { get; set; }
