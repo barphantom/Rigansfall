00822ed98057094d0c7b2c156a693488409bb2a8
adrianpiecek <adrian.piecek@gmail.com>
2025-01-17 01:05:38 +0100

    maps are generated by external tool made in python, controller will only read maps from DB. uploaded maps json for DB

--- Rigansfall.Server/Controllers/MapsController.cs
+++ Rigansfall.Server/Controllers/MapsController.cs
@@ -7,16 +7,12 @@ namespace Rigansfall.Server.Controllers
     [Route("api/[controller]")]
     public class MapsController : ControllerBase
     {
-        [HttpGet("scenariusz{id:int}")]
-        public ActionResult<Map> GetMap()
-        {
-            int mapID = getMapId();
+        //[HttpGet("scenariusz{id:int}")]
+        //public ActionResult<Map> GetMap()
+        //{
 
-            var map = GenerateMap(mapID);
 
-            return Ok(map);
-
-        }
+        //}
 
         //public ActionResult<Maps> GetMaps()
         //{
@@ -27,31 +23,6 @@ namespace Rigansfall.Server.Controllers
         //    };
         //    return Ok(newMap);
         //}
-
-        private Map GenerateMap(int id, string name, int width, int height)
-        {
-            var tiles = Enumerable.Range(0, width)
-                .SelectMany(x => Enumerable.Range(0, height)
-                .Select(y => new Tile
-                {
-                    MapId = id,
-                    X = x,
-                    Y = y,
-                    isWalkable = DetermineWalkability(x, y) // Wywołanie logiki do określenia przechodniości
-                }))
-                .ToList();
-
-            return new Map
-            {
-                mapId = id,
-                mapName = name,
-                Tiles = tiles
-            };
-        }
-        private int getMapId()
-        {
-            return 1; // Na razie zawsze zwraca 1
-        }
 
     }
 }
